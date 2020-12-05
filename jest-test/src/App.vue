a75d4fab18a9426a64d168d35af88bda0630db27
segawa <>
2020-12-06 02:02:19 +0900

    vue、ts、jest導入
    https://github.com/lmiller1990/vtu-next-demo

---
+++ jest-test/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Vue app
+    <Hello @greet="increment" />
+    Count: {{ count }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Hello from './Hello.vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    Hello
+  },
+
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count += 1
+    }
+  }
+})
+</script>
